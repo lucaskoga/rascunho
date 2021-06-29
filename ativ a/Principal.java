adf37528f59216b0f2a921230faf470b689a35a4
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-06-29 16:35:16 -0300

    Create Principal.java

---
+++ ativ a/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Vaga vaga = new Vaga();
+        Vaga vetVaga[] = new Vaga[10];
+        for (int i = 0; i < vetVaga.length; i++){
+            vetVaga[i] = new Vaga();
+        }
+        vaga.a
+
+    }
+}
