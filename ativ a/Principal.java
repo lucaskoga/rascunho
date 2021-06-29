397ff681979572971bf90e8ff114af3eed4e2d42
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-06-29 17:58:45 -0300

    Update Principal.java

--- ativ a/Principal.java
+++ ativ a/Principal.java
@@ -3,10 +3,13 @@ public class Principal {
     public static void main(String[] args) {
         Vaga vaga = new Vaga();
         Vaga vetVaga[] = new Vaga[10];
+        vetVaga[0]= new Vaga();
+        vetVaga[0].addCarro("AAA4444",114.55f);
+
         for (int i = 0; i < vetVaga.length; i++){
-            vetVaga[i] = new Vaga();
+            System.out.println(vetVaga[i]);
         }
-        vaga.a
+
 
     }
 }
